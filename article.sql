85c021f51e3d64d9a9edc04efe97cca01ec33605
zhen <zhen@sonofsky.com>
2016-05-16 14:27:19 +0800

    adding sql

---
+++ article.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 16, 2016 at 02:25 PM
+-- Server version: 5.7.12-0ubuntu1
+-- PHP Version: 7.0.4-7ubuntu2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aipai`
+--
+
+--
+-- Dumping data for table `article`
+--
+
+INSERT INTO `article` (`artid`, `title`, `headimg`, `url`, `edittime`) VALUES
+(1, '导购1', '40.jpg', 'viewfuns.html', '2016-05-15 08:03:25'),
+(2, '导购1', '21.jpg', 'viewfuns.html', '2016-05-16 06:16:17'),
+(3, 'daogou2', '43.jpg', 'viewfuns.html', '2016-05-16 06:16:23'),
+(4, '范德萨', '士大夫', 'viewfuns.html', '2016-05-16 06:16:26');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
