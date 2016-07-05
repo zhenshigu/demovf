c7854efa50ba4ae3fa999355bd3a512ca4f1e03e
zhen <zhen@sonofsky.com>
2016-07-05 13:58:32 +0800

    modify:修改性别左内补为20px

--- application/views/clientm/commonCss.php
+++ application/views/clientm/commonCss.php
@@ -1,1 +1,1 @@
-<link rel="stylesheet" type="text/css" href="http://static.viewfuns.com/static/css/apm.css?v=0.1">
+<link rel="stylesheet" type="text/css" href="http://static.viewfuns.com/static/css/apm.css?v=0.2">
