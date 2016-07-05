9be5dc939c19b2bc744c845c58d8ff9350e1816b
zhen <zhen@sonofsky.com>
2016-07-05 19:06:52 +0800

    fixbug：修补轮播图片地址错误

--- application/views/clientm/Index.php
+++ application/views/clientm/Index.php
@@ -101,7 +101,7 @@ $(document).ready(function() {
 			$(lst).each(function(k, v) {
 				htm += "<li class='picslist' name='is_tj_r' data-tag=" + i + " style='padding-right: 0;'>";
                                
-				htm += '<img src=<?php echo $base_url."static/article/index_img/"?>'+v+'>';
+				htm += '<img src="static.viewfuns.com/article/index_img/'+v+'>';
 				htm += '</li>';
 				$('.ad_btn_wrap').append('<a href="javascript:;"></a>');
                                 i++;
@@ -188,7 +188,7 @@ $(document).ready(function() {
 $(function(){
 	$('.picslist').on('tap',function() {
 	    var picslist_tag=$(this).data('tag');
-	    location.href='<?php echo $base_url."static/article/"?>'+links[picslist_tag];
+	    location.href='static.viewfuns.com/article/'+links[picslist_tag];
 	});
 });
 </script>
