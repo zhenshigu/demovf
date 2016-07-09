b294ce77480864afe9ef069615f334d3fd1203d5
zhen <zhen@sonofsky.com>
2016-07-09 16:37:52 +0800

    网站添加关于我们页面

---
+++ application/views/pc/abountUs.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">     
+    <link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm.min.css">
+</head>
+
+<body>
+    <div class="page-group">
+        <div class="page page-current">
+            <div class="content">
+                <div class="content-block-title">关于我们</div>
+                <div class="card">
+                  <div class="card-content">
+                    <div class="card-content-inner">视范是一款提供摄影相关资讯的app,让你拍照更有范儿.小范君所提供的文章都是棒棒哒,欢迎摄影人士提供文章.
+                        如需合作,请联系小范君.如有侵权,请联系小范君删除.
+                        <br>联系邮箱:<a href="mailto:account@viewfuns.com">account@viewfuns.com</a></div>
+                  </div>
+                </div>
+                <div class="content-block-title">关注我们</div>
+                <div class="card">
+                  <div class="card-content">
+                    <div class="card-content-inner">
+                        微信公众号:iviewfun
+                        <p>微博:视范</p></div>
+                    
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
